f790833923bce855161b1f227ae39df86271639b
Alexandros Vythoulkas <alvythoulkas@gmail.com>
2021-04-01 16:04:10 +0300

    [doc]: correct some docstrings on utils.py

--- utils.py
+++ utils.py
@@ -20,24 +20,24 @@ def add_to_cache(key: tuple[str, bool], value: str) -> None:
 
 
 def key_in_cache(key: tuple[str, bool]) -> bool:
-    """Checks if key, exists in cache."""
+    """Checks if key exists in cache."""
     with Cache(CACHE_FILE) as cache:
         return key in cache
 
 
 def get_cache_value(key: tuple[str, bool]) -> str:
-    """Add key, value pair to cache."""
+    """Get cache value, given its key."""
     with Cache(CACHE_FILE) as cache:
         return cache.get(key)
 
 
 def clear_cache() -> None:
-    """Add key, value pair to cache."""
+    """Clear everything from cache."""
     with Cache(CACHE_FILE) as cache:
         cache.clear()
 
 
 def get_cache_keys() -> list[Cache.iterkeys]:
-    """Add key, value pair to cache."""
+    """Get every key from cache."""
     with Cache(CACHE_FILE) as cache:
         return list(cache.iterkeys())
